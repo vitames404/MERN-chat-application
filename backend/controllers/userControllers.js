47cd9aa33dc7715fc7af9be18ff71c8379f00b39
vitames404 <vitorames@protonmail.com>
2023-05-10 14:15:59 +1200

    Created user search endpoint

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -90,8 +90,16 @@ const authUser = asyncHandler(async (req, res) => {
 
 // /api/user?search=variable
 const allUsers = asyncHandler(async(req, res) => {
-    const keyword = req.query
-    console.log(keyword);
+    const keyword = req.query.search ? {
+        $or: [
+            { name : { $regex : req.query.search, $options: "i" } },
+            { email: { $regex: req.query.search, $options: "i"} },
+        ]
+    }
+    : {};
+    
+    const users = await User.find(keyword).find({ _id: { $ne:req.user._id } });
+    res.send(users);
 })
 
 // Export Everything
