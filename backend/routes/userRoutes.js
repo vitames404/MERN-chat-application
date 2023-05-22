edf9cd56cd79dfa52a0bdb88a5009cf5cafa3904
vitames404 <vitorames@protonmail.com>
2023-05-22 19:54:57 +1200

    User authentication API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,11 +3,13 @@ const express = require('express')
 const router = express.Router();
 const {registerUser, authUser, allUsers} = require("../controllers/userControllers");
 
+const { protect } = require("../middlewares/authMiddleware")
+
 // Route to login page
 router.post('/login', authUser);
 
 // Route to register a new user
 // Route for the user searching API endpoint
-router.route('/').post(registerUser).get(allUsers);
+router.route('/').post(registerUser).get(protect, allUsers);
 
 module.exports = router;
