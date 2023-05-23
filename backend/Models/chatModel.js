ce5c0c82c5c8d34034a67fc21cbde0e51ce48dd8
vitames404 <vitorames@protonmail.com>
2023-05-23 20:20:09 +1200

    Fixed bug witht the chatModel.js

--- backend/Models/chatModel.js
+++ backend/Models/chatModel.js
@@ -1,43 +1,19 @@
-// The chat model contains
+const mongoose = require("mongoose");
 
-// chatName
-// isGroupChat
-// users
-// latestMessage
-// groupAdmin
-
-const mongoose = require('mongoose')
-
-// Define chat model
-const chatModel = mongoose.Schema({
-
-    chatName:{type:String,trim:true},
-    isGroupChat:{type:Boolean, default:false},
-    users:[
-        {
-            // Schemas details are more clear in the mongoose documentation
-
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-
-        }
-    ],
-    latestMessage:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Message",
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
     },
-    groupAdmin:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"User", 
-    },
-},
-
-    {
-        timestamps: true,
-    }
-
+    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
 );
 
-const Chat = mongoose.Model("Chat", chatModel);
+const Chat = mongoose.model("Chat", chatModel);
 
 module.exports = Chat;
